4c185349c4195b73a0987679ed0471b27a9ed734
andrew_swyatenco <swyatenco@mail.ru>
2025-02-13 00:51:22 +0300

    Added creation and saving of pdf document

--- lib/bloc_primary_protocol/new_body.dart
+++ lib/bloc_primary_protocol/new_body.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:new_table_flutter/bloc_primary_protocol/primary_protocol_dialog.dart';
 import 'package:new_table_flutter/bloc_primary_protocol/primary_protocol_event.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 import '../data/models/primary_protocol_model/primary_protocol_model.dart';
 import '../data/models/protocol_name_model/protocol_name_model.dart';
@@ -27,6 +33,12 @@ class NewPrimaryProtocolBody extends StatelessWidget {
           primaryProtocolList != null && primaryProtocolList!.isEmpty
               ? _floatingButton(context)
               : Container(),
+          IconButton(
+            icon: Icon(Icons.save_alt),
+            onPressed: () async {
+              await _generatePdf(context);
+            },
+          ),
         ],
       ),
       body: ListView.builder(
@@ -164,6 +176,121 @@ class NewPrimaryProtocolBody extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Future<void> _generatePdf(BuildContext context) async {
+    final pdf = pw.Document();
+    // Загрузка шрифта
+    final ttf = pw.Font.ttf(await rootBundle.load('assets/fonts/Roboto-Regular.ttf'));
+
+    pdf.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return pw.Padding(
+            padding: const pw.EdgeInsets.all(20),
+            child: pw.Column(
+              crossAxisAlignment: pw.CrossAxisAlignment.start,
+              children: [
+                pw.Text(
+                  'Первичный протокол по шуму',
+                  style: pw.TextStyle(fontSize: 24, font: ttf, fontWeight: pw.FontWeight.bold),
+                ),
+                pw.SizedBox(height: 20),
+                // Добавление таблицы для данных
+                pw.Table(
+                  border: pw.TableBorder.all(),
+                  children: [
+                    // Заголовки таблицы
+                    pw.TableRow(children: [
+                      _buildHeaderCell('Параметр', ttf),
+                      _buildHeaderCell('Значение', ttf),
+                    ]),
+                    // Данные протокола
+                    for (var protocol in primaryProtocolList ?? []) ...[
+                      pw.TableRow(children: [
+                        _buildTableCell('Организация:', ttf),
+                        _buildValueCell(protocol.organizationName, ttf),
+                      ]),
+                      pw.TableRow(children: [
+                        _buildTableCell('Рабочее место:', ttf),
+                        _buildValueCell(protocol.workplace, ttf),
+                      ]),
+                      pw.TableRow(children: [
+                        _buildTableCell('Фамилия работника:', ttf),
+                        _buildValueCell(protocol.familyName, ttf),
+                      ]),
+                      pw.TableRow(children: [
+                        _buildTableCell('Название параметра:', ttf),
+                        _buildValueCell(protocol.parameterName, ttf),
+                      ]),
+                      pw.TableRow(children: [
+                        _buildTableCell('Значение уровня шума №1:', ttf),
+                        _buildValueCell(protocol.parameterValue1, ttf),
+                      ]),
+                      pw.TableRow(children: [
+                        _buildTableCell('Значение уровня шума №2:', ttf),
+                        _buildValueCell(protocol.parameterValue2, ttf),
+                      ]),
+                      pw.TableRow(children: [
+                        _buildTableCell('Значение уровня шума №3:', ttf),
+                        _buildValueCell(protocol.parameterValue3, ttf),
+                      ]),
+                      pw.TableRow(children: [
+                        _buildTableCell('Среднее значение уровня шума:', ttf),
+                        _buildValueCell(protocol.averageConcentration, ttf),
+                      ]),
+                    ],
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+
+    // Получаем директорию для сохранения файла
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/первичный_протокол_по_шуму.pdf');
+
+    // Сохраняем PDF в файл
+    await file.writeAsBytes(await pdf.save());
+
+    // Выводим путь к файлу в консоль
+    print('PDF сохранен по пути: ${file.path}');
+  }
+
+// Функция для создания ячейки заголовка таблицы
+  pw.Widget _buildHeaderCell(String text, pw.Font ttf) {
+    return pw.Padding(
+        padding: const pw.EdgeInsets.all(8.0),
+        child: pw.Align(
+          alignment: pw.Alignment.center,
+          child: pw.Text(text, style: pw.TextStyle(fontWeight: pw.FontWeight.bold, font: ttf)),
+        ));
+  }
+
+// Функция для создания ячейки таблицы с текстом
+  pw.Widget _buildTableCell(String label, pw.Font ttf) {
+    return pw.Padding(
+      padding: const pw.EdgeInsets.all(8.0),
+      child: pw.Align(
+        alignment: pw.Alignment.center,
+        child: pw.Text(label, style: pw.TextStyle(fontWeight: pw.FontWeight.bold, font: ttf)),
+      ),
+    );
+  }
+
+  // Обновленная функция для создания ячейки с значением по центру
+  pw.Widget _buildValueCell(String value, pw.Font ttf) {
+    return pw.Padding(
+      padding: const pw.EdgeInsets.all(8.0),
+      child: pw.Align(
+        alignment: pw.Alignment.center,
+        child: pw.Text(value, style: pw.TextStyle(font: ttf)),
       ),
     );
   }
